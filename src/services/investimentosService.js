03704dfc13f30cc67d37d67955ab060b45dd9ea2
LucasTapa <lucas.dfa@live.com>
2022-07-17 16:08:24 -0300

    feat(component sellstocks): inclusao do endpoint de venda de açoes
    foi implementado o endpoint de venda de ações e a sua regra de negocio

--- src/services/investimentosService.js
+++ src/services/investimentosService.js
@@ -10,11 +10,21 @@ const buyStocks = async ({ codCliente, codAtivo, qtdeAtivo }) => {
     await Wallet.create({ codCliente, codAtivo, qtdeAtivo, valor: null });
 };
 
-
 const sellStocks = async ({ codCliente, codAtivo, qtdeAtivo }) => {
-  
-}
+  const verificaQtd = await Wallet.findByPk(codAtivo, { attributes: ['qtdeAtivo'] });
+  if (qtdeAtivo > verificaQtd.dataValues.qtdeAtivo) {
+    const error = { 
+      status: 422, message: 'Você não tem ações suficientes para serem vendidas' };
+    throw error;
+  }
+  const saldo = verificaQtd.dataValues.qtdeAtivo - qtdeAtivo;
+  if (saldo === 0) await Wallet.destroy({ where: { codCliente, codAtivo } });
+
+  await Wallet
+  .update({ qtdeAtivo: saldo }, { where: { codCliente, codAtivo } });
+};
 
 module.exports = {
   buyStocks,
+  sellStocks,
 };
